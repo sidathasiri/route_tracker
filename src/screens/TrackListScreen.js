ff52a22ff4d5ba859ae0f98b203c80c95b467266
Sidath Asiri <sidath.asiri@syscolabs.com>
2020-08-01 22:49:01 +0530

    configured appllication navigation config

---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const TractListScreen = () => {
+  return <Text style={{ fontSize: 48 }}>TractListScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TractListScreen;
