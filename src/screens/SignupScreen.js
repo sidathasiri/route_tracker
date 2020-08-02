0cd6b00a50226f4342543016a4634047037f2763
Sidath Asiri <sidath.asiri@syscolabs.com>
2020-08-02 23:06:07 +0530

    added state variables in signup

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignUpScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Signup for Tracker</Text>
       </Spacer>
       <Spacer>
-        <Input label='Email' />
+        <Input
+          label='Email'
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize='none'
+          autoCorrect={false}
+        />
       </Spacer>
       <Spacer>
-        <Input label='Password' />
+        <Input
+          label='Password'
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize='none'
+          autoCorrect={false}
+        />
       </Spacer>
       <Spacer>
         <Button
